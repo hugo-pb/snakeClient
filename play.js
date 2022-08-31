4e0bebda530b033afd7774e754aea6267efe7424
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-31 14:51:47 -0600

    moved inputs to their own file and exported

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 console.log("Connecting ...");
 connect();
-// setup interface to handle user input from stdin
-const handleUserInput = function(data) {
-  // your code here
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+
 setupInput();
