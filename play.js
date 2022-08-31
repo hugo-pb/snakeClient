8344874f0b48306127d072af40802cb99a190ff4
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-31 14:34:19 -0600

    setup input

--- play.js
+++ play.js
@@ -1,3 +1,12 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
