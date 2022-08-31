cfb6f70ac483266535aafdeb7bfa6ecef9aa443a
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-31 14:44:51 -0600

    set up for controls stin, only added an exit path

--- play.js
+++ play.js
@@ -2,11 +2,18 @@ const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 // setup interface to handle user input from stdin
-
+const handleUserInput = function(data) {
+  // your code here
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
 };
+setupInput();
